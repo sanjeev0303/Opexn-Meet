fd37aabff72aa025869f2d5bb6c88971eec43f25
sanjeev0303 <sanjeev.081021@gmail.com>
2025-03-05 15:10:20 +0530

    change on old branch

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,8 +9,10 @@ const protectedRoute = createRouteMatcher([
   '/personal-room',
 ]);
 
-export default clerkMiddleware((auth, req) => {
-  if (protectedRoute(req)) auth().protect();
+
+
+export default clerkMiddleware(async (auth, req) => {
+  if (protectedRoute(req)) await auth.protect();
 });
 
 export const config = {
