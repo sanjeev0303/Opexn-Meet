a55af8b2299b1232294f171d828824e051ca24f6
sanjeev0303 <sanjeev.081021@gmail.com>
2025-03-06 06:22:18 +0530

    add logo height

--- src/components/MeetingRoom.tsx
+++ src/components/MeetingRoom.tsx
@@ -50,14 +50,14 @@ const MeetingRoom = () => {
 
     return (
       <div className="border-[5px] lg:border-[10px] md:border-[10px] absolute w-full h-full rounded-lg">
-        <div className="bg-white w-full lg:h-9 md:h-9 h-4  p-2 flex justify-between items-center text-black lg:p-2 md:p-2 rounded-t-[3px] lg:rounded-t-none md:rounded-t-none">
+        <div className="bg-white w-full lg:h-9 md:h-9 h-6  p-2 flex justify-between items-center text-black lg:p-2 md:p-2 rounded-t-[3px] lg:rounded-t-none md:rounded-t-none">
           <div>
             <Image
               src="/images/opexn_logo.png"
               alt="logo"
               width={50}
               height={50}
-              className="rounded-full lg:size-full md:size-full size-5 sm:w-5 sm:h-5"
+              className="rounded-full lg:size-full md:size-full size-10 sm:w-10 sm:h-10"
             />
           </div>
 
